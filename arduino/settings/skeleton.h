5fae4bef7a8680b258040482665be3c621fcd625
rbong <roger.bongers@yahoo.ca>
2015-07-21 16:05:05 -0400

    Updated skeleton configuration.

--- arduino/settings/skeleton.h
+++ arduino/settings/skeleton.h
@@ -1,17 +1,15 @@
 #define SERVOS 1
 #define BUFSIZE 1
 
-uint8_t limit [SERVOS] [2] = {
-    { 0, 0 },
-};
-uint8_t default_pos [SERVOS] = { 0 };
-uint8_t reverse [SERVOS] = { 0 };
+uint8_t limit [SERVOS] [2] = { { 0, 180 }, };
+uint8_t default_pos [SERVOS] = { 0, };
+uint8_t reverse [SERVOS] = { 0, };
 uint8_t pin_offset = 2;
 
 enum
 { 
     VERBOSE = 0,
-    BOARD_ID = 181,
+    BOARD_ID = 0,
 };
 
 int getServoFromID (uint8_t servo_id)
@@ -21,4 +19,22 @@ int getServoFromID (uint8_t servo_id)
         return -1;
     }
     return servo_id;
+}
+
+int getIDFromServo (uint8_t servo_num)
+{
+    if (servo_num >= SERVOS || servo_num < 0)
+    {
+        return -1;
+    }
+    return servo_num;
+}
+
+int getServoPinFromNum (uint8_t servo_num)
+{
+    if (servo_num >= SERVOS || servo_num < 0)
+    {
+        return -1;
+    }
+    return servo_num;
 }
