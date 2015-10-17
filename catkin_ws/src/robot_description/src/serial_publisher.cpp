098ad93c63377bc19f68d9c54db32aed328e6c5c
rbong <roger.bongers@yahoo.ca>
2015-10-17 13:16:31 -0400

    Split serial publisher into two services

---
+++ catkin_ws/src/robot_description/src/serial_publisher.cpp
@@ -0,0 +1,65 @@
+#include <ros/ros.h>
+#include <std_msgs/UInt8.h>
+#include <SerialPort.h>
+
+int main(int argc, char** argv)
+{
+    SerialPort* serial_port;
+    std::string port_name;
+
+    ros::init(argc, argv, "serial_publisher");
+    ros::NodeHandle node;
+    ros::Publisher serial_publisher =
+        node.advertise<std_msgs::UInt8>("inmoov_command", 500);
+    ros::Rate loop_rate(500);
+
+    if (ros::param::get ("/serial_publisher/port_name", port_name))
+    {
+        ROS_INFO ("Port name: %s", port_name);
+    }
+    else
+    {
+        ROS_ERROR ("Could not retrieve port name");
+        return 1;
+    }
+
+    ROS_INFO ("Connecting to serial port");
+    serial_port = new SerialPort (port_name);
+
+    ROS_INFO ("connecting to serial port");
+    serial_port->Open ();
+    serial_port->SetBaudRate (SerialPort::BAUD_9600);
+    serial_port->SetCharSize (SerialPort::CHAR_SIZE_8);
+    serial_port->SetNumOfStopBits (SerialPort::STOP_BITS_1);
+    serial_port->SetParity (SerialPort::PARITY_NONE);
+
+    while (serial_port->IsDataAvailable ())
+    {
+        serial_port->ReadByte ();
+    }
+
+    while (ros::ok ())
+    {
+        std_msgs::UInt8 msg;
+
+        if (! serial_port->IsOpen ())
+        {
+            ROS_ERROR ("Connection to serial port lost");
+            return 1;
+        }
+        if (serial_port->IsDataAvailable ())
+        {
+            msg.data = serial_port->ReadByte ();
+            serial_publisher.publish (msg);
+            ROS_DEBUG ("Recieved byte %u", msg.data);
+        }
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+    }
+
+    serial_port->Close ();
+
+    return 0;
+}
--- catkin_ws/src/robot_description/src/state_publisher.cpp
+++ catkin_ws/src/robot_description/src/state_publisher.cpp
@@ -1,14 +1,16 @@
 #include <string>
+#include <std_msgs/UInt8.h>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include <tf/transform_broadcaster.h>
 
 #include <urdf/model.h>
 
-#include <SerialPort.h>
-
 #include <iostream>
 #include <fstream>
+
+double angle [6] = { 0, 0, 0, 0, 0, 0, };
+double joint_limit [6] [2];
 
 double map (double value, double from_min, double from_max, double to_min, double to_max)
 {
@@ -30,49 +32,58 @@ double map (double value, double from_min, double from_max, double to_min, doubl
     return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min;
 }
 
-int init_serial (SerialPort* serial_port)
+void callback (const std_msgs::UInt8::ConstPtr& msg)
 {
-    ROS_INFO ("Opening port");
+    static int c;
+    static int servo = -1;
 
-    try
+    c = msg->data;
+    ROS_DEBUG ("Byte is %d", c);
+    if (c == 255 || c == 253)
     {
-        serial_port->Open ();
-        serial_port->SetBaudRate (SerialPort::BAUD_9600);
-        serial_port->SetCharSize (SerialPort::CHAR_SIZE_8);
-        serial_port->SetNumOfStopBits (SerialPort::STOP_BITS_1);
-        serial_port->SetParity (SerialPort::PARITY_NONE);
+        c = -1;
     }
-    catch (...)
+    if (servo == -1)
     {
-        ROS_ERROR ("Cannot open. Exiting");
-        exit (1);
+        servo = c;
+        ROS_DEBUG ("Setting servo to %d", servo);
+    }
+    else if (servo > 0 && servo < 6)
+    {
+        ROS_DEBUG ("Setting angle to %g", angle);
+        angle [servo] = map
+            (c, 0, 180, joint_limit [servo] [1], joint_limit [servo] [0]);
+        servo = -1;
+    }
+    else
+    {
+        servo = -1;
     }
 }
 
 int main (int argc, char** argv)
 {
-    int c;
-    int servo = -1;
-
-    double joint_limit [6] [2];
     boost::shared_ptr<const urdf::Joint> joint;
 
     ros::init (argc, argv, "state_publisher");
     ros::NodeHandle n;
-    ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState> ("joint_states", 1);
+    ros::Publisher joint_pub =
+        n.advertise<sensor_msgs::JointState> ("joint_states", 1);
+    ros::Subscriber inmoov_sub =
+        n.subscribe ("inmoov_command", 500, callback);
     tf::TransformBroadcaster broadcaster;
     ros::Rate loop_rate (500);
 
-    SerialPort* serial_port;
+    geometry_msgs::TransformStamped odom_trans;
+    sensor_msgs::JointState joint_state;
 
-    double angle [6] = { 0, 0, 0, 0, 0, 0, };
     const char* joint_name [6] =
     {
         "right_arm_to_wrist", "right_palm_to_thumb", "right_palm_to_index",
         "right_palm_to_medius", "right_palm_to_ring", "right_palm_to_pinky",
     };
 
-    std::string urdf_file, port_name;
+    std::string urdf_file;
     urdf::Model model;
 
     if (ros::param::get ("/state_publisher/urdf_file", urdf_file))
@@ -97,23 +108,7 @@ int main (int argc, char** argv)
         joint_limit [i] [1] = joint->limits->upper;
     }
 
-    if (ros::param::get ("/state_publisher/port_name", port_name))
-    {
-        ROS_INFO ("Port name: %s", port_name);
-    }
-    else
-    {
-        ROS_ERROR ("Could not retrieve port name");
-        return 1;
-    }
-
-    ROS_INFO ("Connecting to serial port");
-    serial_port = new SerialPort (port_name);
-    init_serial (serial_port);
-
     ROS_INFO ("Initializing geometry message");
-    geometry_msgs::TransformStamped odom_trans;
-    sensor_msgs::JointState joint_state;
     odom_trans.header.frame_id = "map";
     odom_trans.child_frame_id = "base_link";
     odom_trans.transform.translation.x = 0;
@@ -121,64 +116,26 @@ int main (int argc, char** argv)
     odom_trans.transform.translation.z = 0;
     odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw (0);
 
-    while (serial_port->IsDataAvailable ())
-    {
-        serial_port->ReadByte ();
-    }
-
     ROS_INFO ("Listening");
     while (ros::ok ())
     {
         joint_state.header.stamp = ros::Time::now ();
         odom_trans.header.stamp = ros::Time::now ();
 
-        if (! serial_port->IsOpen ())
-        {
-            ROS_ERROR ("Connection to serial port lost.");
-            return 1;
-        }
-        if (serial_port->IsDataAvailable ())
-        {
-            ROS_DEBUG ("serial data is available");
-            c = serial_port->ReadByte ();
-            ROS_DEBUG ("byte is %d", c);
-            if (c == 255 || c == 253)
-            {
-                c = -1;
-            }
-            if (servo == -1)
-            {
-                servo = c;
-                ROS_DEBUG ("setting servo to %d", servo);
-            }
-            else if (servo > 0 && servo < 6)
-            {
-                ROS_DEBUG ("setting angle to %g", angle);
-                angle [servo] = map
-                    (c, 0, 180, joint_limit [servo] [1], joint_limit [servo] [0]);
-                servo = -1;
-            }
-            else
-            {
-                servo = -1;
-            }
-        }
-
         joint_state.name.resize (6);
         joint_state.position.resize (6);
         for (int i = 0; i < 6; i++)
         {
-            joint_state.name[i] = joint_name [i];
-            joint_state.position[i] = angle [i];
+            joint_state.name [i] = joint_name [i];
+            joint_state.position [i] = angle [i];
         }
 
         joint_pub.publish (joint_state);
         broadcaster.sendTransform (odom_trans);
 
+        ros::spinOnce ();
         loop_rate.sleep ();
     }
-
-    serial_port->Close ();
 
     return 0;
 }
